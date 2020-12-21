e2df62fab76e0a7a5a40c7e7d838a8aa2b40d168
Frederick PORT <developpeur@stud-ios.fr>
2020-12-21 21:37:51 +0100

    Initial Commit

---
+++ Shared/LesShakrasApp.swift
@@ -0,0 +1,17 @@
+//
+//  LesShakrasApp.swift
+//  Shared
+//
+//  Created by Frederick Port on 21/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct LesShakrasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
